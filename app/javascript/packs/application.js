a1a6c25b75f78bc21bf48fc92a6f9c6f2610131c
Ambar Galan <ambar.galan@gmail.com>
2021-01-15 19:31:38 +0100

    adding js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("components")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -24,11 +25,17 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Internal imports
+import { initPieChart } from '../components/graphs';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initPieChart();
 });
+
+console.log("hello");
+
+
